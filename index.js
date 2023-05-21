01ba5c9f6c4de73300c49f9aaef422e17eea4df1
Quoc Trung <buiquoctrung2002@gmail.com>
2023-05-22 01:36:03 +0700

    add database

--- index.js
+++ index.js
@@ -3,9 +3,34 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const expressHandlebars = require('express-handlebars');
 
 // cau hinh public static folder
 app.use(express.static(__dirname + '/public'))
+
+// cau hinh su dung express-handlebars
+app.engine('hbs', expressHandlebars.engine({
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+    extname: 'hbs',
+    defaultLayout: 'layout'
+}));
+app.set('view engine', 'hbs');
+
+//routes
+app.get('/createTables', (req, res) => {
+    let models = require('./models');
+    models.sequelize.sync().then(()=>{
+        res.send('tables created!');
+    });
+})
+app.get('/', (req, res) =>{
+    res.render('index');
+});
+
+app.get('/:page', (req, res) => {
+    res.render(req.params.page);
+})
 
 // app.get('/', (req, res) => {
 //     res.send('Hello world');
