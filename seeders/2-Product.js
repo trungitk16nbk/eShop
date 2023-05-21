6dcafeb3465118d2833845eeb379284093cb10cf
Quoc Trung <buiquoctrung2002@gmail.com>
2023-05-22 01:37:01 +0700

    update

---
+++ seeders/2-Product.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    let data = [{
+      name: 'Quintiles', 
+      imagePath: '/img/brand-1.png'
+    }, {
+      name: 'IndiaCapital',
+      imagePath: '/img/brand-2.png'
+    }, {
+      name: 'PaperlinX', 
+      imagePath: '/img/brand-3.png'
+    }, {
+      name: 'InfraRed', 
+      imagePath: '/img/brand-4.png'
+    }, {
+      name: 'Erlang', 
+      imagePath: '/img/brand-5.png'
+    }, {
+      name: 'Sport England', 
+      imagePath: '/img/brand-6.png'
+    }, {
+      name: 'Quintiles', 
+      imagePath: '/img/brand-1.png'
+    },
+  ];
+
+
+  data.forEach(item => {
+    item.createdAt = Sequelize.literal('NOW()');    
+    item.updatedAt = Sequelize.literal('NOW()');    
+  });
+  await queryInterface.bulkInsert('Products', data, {});
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Products', null, {});
+  }
+};
